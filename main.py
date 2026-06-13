f04a3c34885f16334324f4407499082c9667d532
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-06-13 21:22:54 +0100

    ✨feat: select "english" option in "change language" pop-up

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
+import time
 from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
-URL = "https://www.python.org" # set the URL
+URL = "https://ozh.github.io/cookieclicker/" # set the URL
+PURCHASE_INTERVAL = 15 # set the purchase interval in seconds
+RUN_TIME = 5 * 60 # 5 minutes in seconds
 
 chrome_options = webdriver.ChromeOptions() # Create a new option object
 chrome_options.add_experimental_option("detach", True) # Attach the driver to the background
 
 driver = webdriver.Chrome(options=chrome_options) # Instantiate the driver
 driver.get(URL) # Open the URL
+wait = WebDriverWait(driver, 5) # Create a new wait object
 
-items = driver.find_elements(By.CSS_SELECTOR, ".shrubbery ul.menu li") # Find all the items
-events = {}
-i=0
-for li in items: # Loop through the items
-    times = li.find_elements(By.TAG_NAME, "time") # Find all the times
-    if not times: # If there are no times
-        continue # Skip this item
+def change_language():
+    """
+    Change the language to English
+    """
+    try: # try to change the language
+        wait.until(EC.presence_of_element_located((By.ID, "prompt"))) # Wait for the change language popup to appear
 
-    date = times[0].text # Get the date
-    a = li.find_element(By.TAG_NAME, "a") # Find the link
-    title = a.text # Get the title
+        lang_select = wait.until(EC.element_to_be_clickable((By.ID, "langSelect-EN"))) # Wait for the language select button to be clickable
+        lang_select.click() # Click the language select button
 
-    events[i] = {"time": date, "name": title} # Add the event to the list
-    i+=1
+    except TimeoutException: # if the language is already set to English
+        print("Language already set to English")
 
-print(events)
-
-# try: # Try to click the button
-#     btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.a-button-text"))) # Wait for the button to be clickable
-#     ActionChains(driver).move_to_element(btn).click().perform()  # Click the button
-#     print("Button clicked")
-#
-# except: # If the button is not found
-#     print("Button not found")
-#
-#
-# try: # now try to get the price
-#     price_euro = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "a-price-whole"))) # Wait for the price element to appear
-#     price_cents = driver.find_element(By.CLASS_NAME, "a-price-fraction")  # Find the price element
-#     print(price_euro.text + "." + price_cents.text)
-#
-# except: # If the price is not found
-#     print("Price not found")
-
-#driver.close() # Close the driver
-driver.quit() # Quit the driver
+first_interaction = True # set the first interaction to true
+start_time = time.time() # get the start time
+last_purchase = time.time() # set the last purchase time to now
+while True: # keep the game running
+    if first_interaction: # if it's the first interaction
+        change_language() # select english option in "change language" pop-up
+        first_interaction = False # set the first interaction to false
